e6bee70f9cc35dce80fe9669195ebccc98a95ffc
semuhphor <bob.flanders@gmail.com>
2012-08-06 20:26:32 -0400

    Added user factory and tests for show page

--- Gemfile
+++ Gemfile
@@ -12,5 +12,6 @@ end
 group :test do
 	gem 'rspec', '2.10.0'
 	gem 'webrat'
+	gem 'factory_girl_rails', '1.0'
 end
 
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define  :user do |user|
+  user.name                     "someone else"
+  user.email                    "someemail@email.com"
+  user.password                 "foobar"
+  user.password_confirmation    "foobar"
+ end
