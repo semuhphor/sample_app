15f5ee11d83c865d7470ec8b3a5b1a512dc86823
semuhphor <bob.flanders@gmail.com>
2012-08-06 21:05:46 -0400

    Added more tests and a gravatar.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,6 +18,22 @@ describe UsersController do
       get :show, :id => @user
       assigns(:user).should == @user 
     end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+    
+    it "should have3 the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+    
+    it "should have a profile image" do 
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+    
   end
 
   describe "GET 'new'" do
@@ -28,8 +44,7 @@ describe UsersController do
 
     it "should have the right title" do
       get :new
-      response.should have_selector(
-      		"title", :content => "Sign up")
+      response.should have_selector("title", :content => "Sign up")
     end
   end
 end
